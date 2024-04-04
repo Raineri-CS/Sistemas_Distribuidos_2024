5a029d32f0132e3c62b40a111f68fc1ce094e9dd
Raineri-CS <Raineri-CS@github.com>
2024-04-04 14:18:38 -0300

    ClientHandler : fixed exceptiong when trying to read from a closed socket

--- src/main/java/com/sisdist/server/ClientHandler.java
+++ src/main/java/com/sisdist/server/ClientHandler.java
@@ -5,48 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class ClientHandler extends Thread{
+public class ClientHandler extends Thread {
     private final Socket clientSocket;
     private Thread handlerThread;
 
 
-    public ClientHandler(Socket socket){
+    public ClientHandler(Socket socket) {
         this.clientSocket = socket;
         this.handlerThread = null;
     }
-    public ClientHandler(Socket socket, Thread ownThread){
+
+    public ClientHandler(Socket socket, Thread ownThread) {
         this.clientSocket = socket;
         this.handlerThread = ownThread;
     }
 
-    public void read(){
-        try (BufferedReader buf = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())))){
-            // TODO fazer o processamento dos dados que vieram
-            System.out.println(buf);
-        } catch (IOException e){
+    public void read() {
+        try {
+            if (!clientSocket.isClosed()) {
+                BufferedReader buf = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
+                // TODO fazer o processamento dos dados que vieram
+                System.out.println(buf);
+            } else {
+                System.out.println("Socket is closed, cannot read.");
+            }
+        } catch (IOException e) {
             System.out.println("Error in ClientHandler read()" + e);
         }
     }
 
-    public void terminate(){
+    public void terminate() {
         try {
             clientSocket.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Couldnt close socket in ClientHandler terminate()" + e);
         }
     }
 
-    public Socket getSocket(){
+    public Socket getSocket() {
         return clientSocket;
     }
 
-    public Thread getThread() { return handlerThread; }
+    public Thread getThread() {
+        return handlerThread;
+    }
 
-    public void setThread(Thread ownThread) { this.handlerThread = ownThread;}
+    public void setThread(Thread ownThread) {
+        this.handlerThread = ownThread;
+    }
+
     @Override
-    public void run(){
+    public void run() {
         // TODO o loop da thread aqui
-        while(true){
+        while (true) {
             read();
         }
     }
