b3a2f172af1aa800604bd41d9ada76533f9e7a4e
Raineri-CS <Raineri-CS@github.com>
2024-04-30 05:35:27 -0300

    Hi, its 5 am

--- src/main/java/com/sisdist/server/ClientHandler.java
+++ src/main/java/com/sisdist/server/ClientHandler.java
@@ -4,6 +4,11 @@ import java.io.*;
 import java.net.Socket;
 import java.util.Collections;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
@@ -14,6 +19,22 @@ import com.sisdist.server.messages.Message;
 
 public class ClientHandler implements Runnable {
     private final Socket clientSocket;
+    private final String magicWord = "DISTRIBUIDOS";
+
+    private Algorithm algorithm = Algorithm.HMAC256(magicWord);
+
+    private String genToken(int id, String role) throws JWTCreationException {
+        try {
+            return JWT.create().withClaim("id", String.valueOf(id)).withClaim("role", role).sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new JWTCreationException("Falha ao criar JWT", exception);
+        }
+    }
+
+    private DecodedJWT verifyToken(String token){
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        return verifier.verify(token);
+    }
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
@@ -30,26 +51,28 @@ public class ClientHandler implements Runnable {
 
     private void processMessage(String message) {
         Gson gson = new Gson();
-        OutMessage out;
+        OutMessage out = null;
         try {
             JsonObject jsonObject = JsonParser.parseString(message).getAsJsonObject();
-            Message json;
+            Message json = null;
             String operation = jsonObject.get("operation").getAsString();
-            switch(operation){
+            switch (operation) {
                 case "LOGIN_CANDIDATE":
                 case "LOGIN_RECRUITER":
                     json = gson.fromJson(jsonObject, IN_TWO_PARAMETERS.class);
+                    // Para qualquer login, gerar o token para assinar o pacote
+
                     break;
                 case "LOGOUT_CANDIDATE":
                 case "LOGOUT_RECRUITER":
                     json = gson.fromJson(jsonObject, IN_THREE_PARAMETERS.class);
+                    // TODO as mensagens atribuindo pra out
                     break;
                 default:
-                    throw new IllegalArgumentException("Operação desconhecida: " + operation);
+                    out = new OutMessage("NAO_EXISTE", null , null);
             }
             if (json == null) throw new JsonParseException("");
 
-            out = new OutMessage(json.getOperation(), "ACCEPTED", Collections.emptyMap());
         } catch (Exception e) {
 //            e.printStackTrace();
             out = new OutMessage(null, "REJECTED", Collections.emptyMap());
@@ -57,7 +80,6 @@ public class ClientHandler implements Runnable {
 
         String outJson = gson.toJson(out);
 
-        outJson = outJson.concat("\n");
         try {
 //            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( clientSocket.getOutputStream() ) );
             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
@@ -88,4 +110,6 @@ public class ClientHandler implements Runnable {
     public void run() {
         read();
     }
+
+
 }
